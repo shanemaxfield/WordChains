0fb6770153e39ff9f893b623cc6d7e6b49b9681e
Shanemaxfield <shanemaxfield18@gmail.com>
2025-04-27 11:17:27 -0600

    Initial Commit

---
+++ Word_Chains/Word_ChainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ChainsApp.swift
+//  Word_Chains
+//
+//  Created by Shane Maxfield on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Word_ChainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Word_ChainsTests/Word_ChainsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ChainsTests.swift
+//  Word_ChainsTests
+//
+//  Created by Shane Maxfield on 4/27/25.
+//
+
+import Testing
+@testable import Word_Chains
+
+struct Word_ChainsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
